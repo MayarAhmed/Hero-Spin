4612776475148d0a785b14fff7073c3c9e6f2e96
unknown <mayarahmed72@gmail.com>
2022-11-09 16:07:08 +0200

    intialize the hero app

---
+++ hero-spin/pages/heroPage.jsx
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+
+export default function HeroPage () {
+  return (
+    <h1>Hero Page</h1>
+  )
+}
---
+++ hero-spin/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Head>
+        <h1>Hero Spin</h1>
+        <meta name="description" content="Your Hero Watchlist" />
+        <link rel="stylesheet" href="/styles.css"/>
+      </Head>
+     
+    </div>
+  )
+}
